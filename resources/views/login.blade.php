677606b7c3251bfbc65ed5cbf1b954f1f84f5d77
Luis Toro <luis.toro.f@usach.cl>
2022-06-22 17:32:49 -0400

    [Luis Toro] - Pequeños cambios del front de login y register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,42 +11,49 @@
 
 	<!--<link href="{{ asset('css/login.css') }}" rel="stylesheet">-->
 </head>
-
+<style>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+</style>
 <body>
 	@include('includes.navbar')
 	<div class="row">
-		<div class="col-3"></div>
-		<div id="colCentral" class="col-3">
-			<div class="wrapper">
-				<div class="title">Iniciar Sesión</div>
-				@if(session('status'))
-				<br>
-				<ul>
-					<li>{{ session('status') }}</li>
-				</ul>
-				@endif
-				@if($errors->any())
-				<br>
-				<ul>
-					@foreach($errors->all() as $error)
-					<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-				@endif
-				<form method="POST" action="/login">
-					<div class="field">
-						<input id="email" name=email type="text" required placeholder="Email">
-					</div>
-					<div class="field">
-						<input id="password" name=password type="password" required placeholder="Password">
-					</div>
-					<div class="text-center">
-						<input id="botonLog" class="btn btn-outline-success" type="submit" value="Login">
-					</div>
-					<div class="signup-link">¿No eres miembro? <a href="/register">Regístrate aquí</a></div>
-				</form>
+		<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="card px-5 py-5" >
+					<div class="col-md-12'" >Iniciar Sesión</div>
+					@if(session('status'))
+					<br>
+						<ul>
+							<li>{{ session('status') }}</li>
+						</ul>
+					@endif
+					@if($errors->any())
+					<br>
+						<ul>
+							@foreach($errors->all() as $error)
+							<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					@endif
+					<form method="POST" action="/login">
+						<div class="field" >	
+							<input id="email" name=email type="text" required placeholder="Email">
+						</div>
+						<div class="field">
+							<input id="password" name=password type="password" required placeholder="Password">
+						</div>
+						<div class="text-center">
+							<input id="botonLog" class="btn btn-outline-success" type="submit" value="Login">
+						</div>
+						<div class="signup-link">¿No eres miembro? <a href="/register">Regístrate aquí</a>
+						</div>
+					</form>
+				</div>
 			</div>
-		</div>
 		<div class="col-3"></div>
 	</div>
 	@include('includes.footer')
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,14 +5,22 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Registro</title>
 
 	<!--<link href="{{ asset('css/register.css') }}" rel="stylesheet">-->
 </head>
 
+<style>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+</style>
+
 <body>
+	
 	<!-- Registro -->
 	@if(session('status'))
 	<br>
@@ -28,27 +36,33 @@
 		@endforeach
 	</ul>
 	@endif
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<div class="card" >
+				<form class="login" method="POST" action="/users/create">
+					<h5 style="color:green; font-size:20px;">Registro de nuevo usuario</h5>
 
-	<form class="login" method="POST" action="/users/create">
-		<h5>Registro de nuevo usuario</h5>
-		<div><input class="controls" type="text" id="nickname" name="nickname" placeholder="Ingrese un nombre de usuario" required></div>
+					<div><input class="controls" type="text" id="nickname" name="nickname" size="45" placeholder="Ingrese un nombre de usuario" required></div>
 
-		<div><input class="controls" type="text" id="password" name="password"  placeholder="Ingrese una contraseña" required></div>
+					<div><input class="controls" type="text" id="password" name="password" size="45" placeholder="Ingrese una contraseña" required></div>
 
-		<div><input class="controls" type="text" id="birth_date" name="birth_date"  placeholder="Ingrese su fecha de nacimiento: AAAA-MM-DD" required></div>
-		
-		<div><input class="controls" type="text" id="email" name="email"  placeholder="Ingrese su email" required></div>
+					<div><input class="controls" type="text" id="birth_date" name="birth_date" size="45" placeholder="Ingrese su fecha de nacimiento: AAAA-MM-DD" required></div>
 
-		<input id="boton-registro" class="btn btn-outline-success" type="submit" name="" value="Registrarse">
+					<div><input class="controls" type="text" id="email" name="email" size="45" placeholder="Ingrese su email" required></div>
 
+					<a class="btn btn-outline-success" href="/home">Cancelar</a>
+					
+					<input id="boton-registro" class="btn btn-outline-success" type="submit" name="" value="Registrarse">
+
+
+			
+
+
+				</form>
+				</div>
 		</div>
-		<div class="text-center">
-			<a class="btn btn-outline-success" href="/home">Cancelar</a>
-		</div>
-	</form>
 
-	@include('includes.footer')
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-		crossorigin="anonymous"></script>
+		@include('includes.footer')
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
