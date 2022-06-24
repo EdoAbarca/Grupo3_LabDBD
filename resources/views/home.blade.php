3b15c597c6303db77433231a49f7b2c19a4a382d
Luis Toro <luis.toro.f@usach.cl>
2022-06-24 19:26:12 -0400

    [Luis Toro] - Empezando el front de Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,29 +13,51 @@
 
 <style>
 
-.outer {
-  border: 5px solid black;
+body{
+  background: #f1f1f1;        /* color de fondo*/
 }
 
-
-
-.box {
-  width: 350px;
-  height: 150px;
-  margin: 10px;
-  padding: 25px;
-  border: 5px solid black;
+.menu{
+  list-style: none;           /* le quita los puntos a la "lista"*/    
+  line-height: 42px;          /* espacio entre los titulos*/
+  /*outline: 1px solid blue;*/
+  padding-left: 0;            /* quita el borde de la izquierda*/
+  width: 250px;               /* tamaño de la caja*/
 }
 
+.menu a{
+  color: #111;                /* color de las letras*/
+  text-decoration: none;      /* le quita el subrayado*/
+  text-transform: uppercase;  /* todo en mayusuculas*/
+}
+
+.menu li{
+  /*outline: 1px solid green;*/
+  /*border-left: 3px solid rgba(255,255,255,.2); color del borde*/
+  box-shadow: 3px 0 rgb(49,48,96) inset;
+  padding-left: 1.5em;                        
+  margin-bottom: 5px;                         /* margen del botón*/
+}
 
 </style>
 
 
-<body style="margin-bottom:22px">
+<body>
 
 
   @include('includes.navbar')
-  <section><div class="box">caja.</div></section>
+  <ul class="menu">
+    <li><a href="#">Nombre plataforma</a></li>
+    <p>Descubre Música</p>
+    <li><a href="#">Canciones más escuchadas</a></li>
+    <li><a href="#">Buscador</a></li>
+    <p>Tu Biblioteca de música</p>
+    <li><a href="#">Canciones que te gustan</a></li>
+    <li><a href="#">Lista de reproducción 1</a></li>
+    <li><a href="#">Lista de reproducción 1</a></li>
+    <li><a href="#">Lista de reproducción 1</a></li>
+  </ul>
+
   <!-- Aquí irá lo que se verá al montar la página web, será el punto de partida para realizar las distintas acciones-->
   @include('includes.footer')
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
