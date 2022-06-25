84355c61776a56329e9db15f10434a53b909251c
Luis Toro <luis.toro.f@usach.cl>
2022-06-25 01:23:34 -0400

    [Luis Toro] - Arreglo de merge home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,28 +15,39 @@
 
 body{
   background: #f1f1f1;        /* color de fondo*/
+  font-family: "Century Gothic";
 }
 
 .menu{
   list-style: none;           /* le quita los puntos a la "lista"*/    
   line-height: 42px;          /* espacio entre los titulos*/
+  margin: .5em;                /* separación con el borde izquierdo*/
   /*outline: 1px solid blue;*/
   padding-left: 0;            /* quita el borde de la izquierda*/
-  width: 250px;               /* tamaño de la caja*/
+  width: 16em;               /* tamaño de la caja*/
+  /*font-size: 12px;*/
 }
 
 .menu a{
   color: #111;                /* color de las letras*/
+  display: block;             /* todo el box es "cliqueable", no solo donde está la palabra*/
   text-decoration: none;      /* le quita el subrayado*/
-  text-transform: uppercase;  /* todo en mayusuculas*/
+  /*text-transform: uppercase;   todo en mayusuculas*/
 }
 
 .menu li{
   /*outline: 1px solid green;*/
   /*border-left: 3px solid rgba(255,255,255,.2); color del borde*/
-  box-shadow: 3px 0 rgb(49,48,96) inset;
+  box-shadow: 7px 0 rgb(49,48,96) inset;
   padding-left: 1.5em;                        
   margin-bottom: 5px;                         /* margen del botón*/
+  --webkit-transition: all.5s;                /* transición*/
+  -o-transition: all.5s;
+  transition: all.5s;
+}
+
+.menu li:hover{                               /* lo hace cuando pasas el mouse por encima*/
+  box-shadow: 16em 7px 0 rgb(49,48,96) inset; /* tamaño y color de la barra que aparece cuando pasas el mouse*/
 }
 
 </style>
