37c49837f93be2fa77dea438deafa153eb501e31
Luis Toro <luis.toro.f@usach.cl>
2022-06-26 18:45:58 -0400

    [Luis Toro] - Grids de Home correctas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,86 +12,82 @@
 </head>
 
 <style>
+  body {
+    background: #f1f1f1; /* color de fondo*/
+    font-family: "Century Gothic";
+  }
 
-body{
-  background: #f1f1f1;        /* color de fondo*/
-  font-family: "Century Gothic";
-}
+  .menu {
+    list-style: none; /* le quita los puntos a la "lista"*/
+    line-height: 42px; /* espacio entre los titulos*/
+    /*margin: 0em; /* separación con el borde izquierdo*/
+    /*outline: 1px solid blue;*/
+    padding-left: 0; /* quita el borde de la izquierda*/
+    width: 16em; /* tamaño de la caja*/
+    font-size: 15px;
+  }
 
-.menu{
-  list-style: none;           /* le quita los puntos a la "lista"*/    
-  line-height: 42px;          /* espacio entre los titulos*/
-  margin: .5em;                /* separación con el borde izquierdo*/
-  /*outline: 1px solid blue;*/
-  padding-left: 0;            /* quita el borde de la izquierda*/
-  width: 16em;               /* tamaño de la caja*/
-  /*font-size: 12px;*/
-}
+  .menu a {
+    color: black; /* color de las letras*/
+    display: block; /* todo el box es "cliqueable", no solo donde está la palabra*/
+    text-decoration: none; /* le quita el subrayado*/
+    /*text-transform: uppercase;   todo en mayusuculas*/
+  }
 
-.menu a{
-  color: #111;                /* color de las letras*/
-  display: block;             /* todo el box es "cliqueable", no solo donde está la palabra*/
-  text-decoration: none;      /* le quita el subrayado*/
-  /*text-transform: uppercase;   todo en mayusuculas*/
-}
-
-.menu li{
-  /*outline: 1px solid green;*/
-  /*border-left: 3px solid rgba(255,255,255,.2); color del borde*/
-  box-shadow: 7px 0 rgb(49,48,96) inset;
-  padding-left: 1.5em;                        
-  margin-bottom: 5px;                         /* margen del botón*/
-  --webkit-transition: all.5s;                /* transición*/
-  -o-transition: all.5s;
-  transition: all.5s;
-}
-
-.menu li:hover{                               /* lo hace cuando pasas el mouse por encima*/
-  box-shadow: 16em 7px 0 rgb(49,48,96) inset; /* tamaño y color de la barra que aparece cuando pasas el mouse*/
-}
-
-.grid {
-  display: grid;
-  grid-template-columns: repeat(3,1fr);
-  gap: 1em;
-  grid-auto-rows: minmax(200px,auto);
-
-  justify-items: stretch;
-}
-
-.grid > div{
-  background: #6148cf;
-  padding: 1em;
-}
-
-.grid > div:hover{
-  border: 1px solid #f1c40f;
-}
-
-.element1{
-  grid-row: 1/3;
-}
-
-.element3{
-  grid-column: 2/4;
-}
+  .menu li {
+    /*outline: 1px solid green;*/
+    /*border-left: 3px solid rgba(255,255,255,.2); color del borde*/
+    box-shadow:  12px 0 rgb(49, 48, 96) inset;
+    padding-left: 1em;
+    margin-bottom: 16px; /* margen del botón*/
+    /*--webkit-transition: all.3s; /* transición
+    -o-transition: all.3s;
+    transition: all.3s;*/
+  }
+  
+  .menu li:hover {
+    /* lo hace cuando pasas el mouse por encima*/
+    box-shadow: 16em 1px 0 rgb(49, 48, 96) inset;
+    /* tamaño y color de la barra que aparece cuando pasas el mouse*/
+  }
 
 
 
-/*.nested-grid{
-  display: grid;
-  grid-template-columns: repeat(3,1fr);
-  grid-template-rows: 100px;
-  gap: 1em
-}
+  .grid {
+    display: grid;
+    grid-template-columns: /*repeat(5,1fr);*/ 260px;
+    /*gap: 1em; 
+    /*grid-template-rows: 700px;*/
 
-.nested.grid > div{
-  border: 1px solid black;
-  background: #f1c40f;
-  padding: 1em;
-}*/
+    /*justify-items: stretch;*/
+  }
 
+  .grid>div {
+    background: #f1f1f1;
+    /*padding: 1em;*/
+  }
 
+  /*.grid>div:hover {
+    border: 1px solid #6148cf;
+  }*/
+
+  .element1 {
+    grid-row: 1/38;
+
+    /*align-self: stretch;*/
+    /*grid-column: 1/2;*/
+  }
+
+  .element2 {
+    grid-row: 2/38;
+    grid-column: 2/3;
+  }
+
+  .element3 {
+    justify-self: end;
+    /*|grid-column: 2/3;
+  grid-column: 2/3;*/
+  }
 </style>
 
 
@@ -105,15 +101,42 @@ body{
   <a class="btn btn-outline-success" href="/playlist">Lista reproducción</a>
   @endguest
   @auth
-    <div class="grid">
-      <div class="element1">element1</div>
-      <div class="element2">element2</div>
-      <div class="element3">element3</div>
-
+  <div class="grid">
+    <div class="element1">
+      <ul class="menu">
+        <p>Nombre plataforma</p>
+        <p>Descubre Música</p>
+        <li><a href="#">Canciones más escuchadas</a></li>
+        <li><a href="#">Buscador</a></li>
+        <p>Tu Biblioteca de música</p>
+        <li><a href="#">Canciones que te gustan</a></li>
+        <li><a href="#">Lista de reproducción 1</a></li>
+        <li><a href="#">Lista de reproducción 2</a></li>
+        <li><a href="#">Lista de reproducción 3</a></li>
+      </ul>
     </div>
+    <div class="element2">
+      @foreach($songs as $song)
+      <div class="col-4 d-flex justify-content-center">
+        <div class="card" style="width: 10rem;">
+        <img src="https://cdn.pixabay.com/photo/2022/06/21/21/15/audio-7276511_960_720.jpg">
+        <div class="card-body">
+          <h5 class="card-title">{{$song->song_name}}</h5>
+          <a href="#" class="btn btn-primary">Ir a la canción</a>
+
+        </div>
+        </div>
+      </div>
+
+    @endforeach
+    </div>
+    <div class="element3">
+      <a class="btn btn-outline-success" href="/profile">Perfil</a></div>
+
+  </div>
 
 
-    <!--<ul class="menu">
+  <!--<ul class="menu">
       <li><a href="#">Nombre plataforma</a></li>
       <p>Descubre Música</p>
       <li><a href="#">Canciones más escuchadas</a></li>
